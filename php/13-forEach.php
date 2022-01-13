a85b92012a8df04638c1f2a1e79609fc9d88ad42
Mahdiyar Sahbaee Ahmadi <78661695+mahdiyarsoft@users.noreply.github.com>
2022-01-14 01:11:49 +0330

    Create 13-forEach.php

---
+++ php/13-forEach.php
@@ -0,0 +1,22 @@
+<?php
+  //  foreach ($array as $value){
+  //    code to be executed;
+  //  }
+
+  # for Example=>
+
+  $person = array(“Mahdiyar”, “lang”,2022);
+
+  foreach ($person as $profile)
+  {
+    echo $profile.”<br/>”;
+  }
+  
+  # code Output=>
+
+  // Mahdiyar
+  // lang
+  // 2022
+?>
+
+?>
