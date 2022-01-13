5c8575bd1effd86625ba822675f3739338fd06a2
Mahdiyar Sahbaee Ahmadi <78661695+mahdiyarsoft@users.noreply.github.com>
2022-01-14 00:24:19 +0330

    Update 3-variables.php

--- php/3-variables.php
+++ php/3-variables.php
@@ -2,10 +2,13 @@
 
 $num1 = 15;
 $num2 = 40;
+$userName = "MahdiyarSoft";
 
 $result = $num1 + $num2;
 
 echo $result;
+echo $userName;
 
-// This code => Print 65
+// This code => Print 65 & Print "MahdiyarSoft".
+
 ?>
