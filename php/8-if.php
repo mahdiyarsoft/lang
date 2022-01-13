775b539cfe648b992dac0ddb8f9b059da4c08a9c
Mahdiyar Sahbaee Ahmadi <78661695+mahdiyarsoft@users.noreply.github.com>
2022-01-14 00:50:49 +0330

    Create 8-if.php

---
+++ php/8-if.php
@@ -0,0 +1,18 @@
+<?php 
+  if(condition)
+  {
+    #codes...
+  }elseif{
+    #codes...
+  }else{
+    #codes...
+  }
+// for example =>
+  $num = 313;
+  if($num == 313){
+    echo 'True';
+    echo $num;
+  }else{
+    echo 'False'
+  }
+?>
