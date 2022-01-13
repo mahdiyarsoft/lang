31b21da842f83334c1d1edc9c00f073cc8d3ccf1
Mahdiyar Sahbaee Ahmadi <78661695+mahdiyarsoft@users.noreply.github.com>
2022-01-14 01:07:37 +0330

    Create 12-doWhile.php

---
+++ php/12-doWhile.php
@@ -0,0 +1,22 @@
+<?php
+  //  do{
+  //   code to be executed;
+  //  }
+  // while (condition);
+
+  // for Example =>
+
+$i=1;
+do{
+echo “The number is ” . $i . “<br>”;
+$i++;
+}
+while ($i<=5);
+
+#code Output =>
+// The number is 1
+// The number is 2
+// The number is 3
+// The number is 4
+// The number is 5
+?>
