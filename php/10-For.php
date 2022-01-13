e608655b21c8423aafff41ea6b2dbfb5e08fb0ca
Mahdiyar Sahbaee Ahmadi <78661695+mahdiyarsoft@users.noreply.github.com>
2022-01-14 01:00:16 +0330

    Create 10-For.php

---
+++ php/10-For.php
@@ -0,0 +1,20 @@
+
+<?php 
+
+//  for (initialization; condition; increment){
+//    code to be executed;
+//  }
+
+
+// for example =>
+for ($i=1; $i<=5; $i++){
+echo “The Number is: “ . $i . ”<br/>”;
+}
+
+// codes Output => 
+   // The number is 2
+   // The number is 3
+   // The number is 4
+   // The number is 5
+ 
+?>
