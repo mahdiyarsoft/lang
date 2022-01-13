1e36fb350d6f6e95adc31e5e6358baa66e29d383
Mahdiyar Sahbaee Ahmadi <78661695+mahdiyarsoft@users.noreply.github.com>
2022-01-14 00:45:16 +0330

    Create 7-dataTypes.php

---
+++ php/7-dataTypes.php
@@ -0,0 +1,10 @@
+<?php
+
+  $number1 = 100;
+  $number2 = 100.0;
+  $userName = ”Hello World”;
+
+  var_dump($number1,$number2,$userName); // Similar to console.log() in javascript
+
+  // code output => int(100) float(100) string(11) “Hello World”
+?>
