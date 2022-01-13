ed08c696683296ea52d852106a3a12268b7bd32f
Mahdiyar Sahbaee Ahmadi <78661695+mahdiyarsoft@users.noreply.github.com>
2022-01-14 00:39:42 +0330

    Create 6-echo-print.php

---
+++ php/6-echo-print.php
@@ -0,0 +1,16 @@
+<?php
+/* 
+1. Echo is a term used to display output. This phrase can be used  without or with parentheses.
+2. The echo can adopt several strings that are separated by (,).
+3- echo does not return any value.
+4- Echo works faster than print.
+*/
+
+// for example =>
+
+$name=”Mahdi”;
+echo $name;
+//or
+echo ($name);
+
+?>
