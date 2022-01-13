019fe68c599d32732dac1423c6b3e880e006361a
Mahdiyar Sahbaee Ahmadi <78661695+mahdiyarsoft@users.noreply.github.com>
2022-01-14 00:54:36 +0330

    Create 9-Switch.php

---
+++ php/9-Switch.php
@@ -0,0 +1,22 @@
+<?php
+  $index = 0;
+  switch ($index){
+    case 0:
+
+    echo $index."equals 0“;
+
+    break;
+
+    case 1:
+
+    echo $index."equals 1“;
+
+    break;
+
+    case 2:
+
+    echo $index."equals 2“;
+
+    break;
+  }
+?>
