62c395ca19b263912c8f9fce69297cf1d58bd046
Mahdiyar Sahbaee Ahmadi <78661695+mahdiyarsoft@users.noreply.github.com>
2022-01-14 00:30:32 +0330

    Create 4-SuperGlobal-Var.php

---
+++ php/4-SuperGlobal-Var.php
@@ -0,0 +1,15 @@
+<?php
+  $_GET[“ ”];
+  $_POST[“ ”];
+  $_REQUEST[“ ”];
+  $_FILES[“ ”];
+  $_SESSION[“ ”];
+  $_COOKIE[“ ”];
+  $_Server[“ ”];
+// For Example=>
+  $_SERVER[“SERVER_PORT”];
+
+  $_SERVER[“SERVER_NAME”];
+
+  $_SERVER[“REQUEST_URI”];
+?>
