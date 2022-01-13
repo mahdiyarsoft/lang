6c3b39ad72b3f7ff21d15dc8c40405b14f76261b
Mahdiyar Sahbaee Ahmadi <78661695+mahdiyarsoft@users.noreply.github.com>
2022-01-14 01:03:34 +0330

    Create 11-While.php

---
+++ php/11-While.php
@@ -0,0 +1,18 @@
+<?php
+ //  while (condition){
+ //    code to be executed;
+ //  }
+// for Example =>
+
+$i=1;
+  while($i<=5){
+  echo “The number is ” . $i . “<br>”;
+  $i++;
+}
+#code Output =>
+// The number is 1
+// The number is 2
+// The number is 3
+// The number is 4
+// The number is 5
+?>
