cd176f71ee909bcaf16a6e9273f2eededa95e187
Mahdiyar Sahbaee Ahmadi <78661695+mahdiyarsoft@users.noreply.github.com>
2022-01-14 00:18:23 +0330

    Create 2-comment.php

---
+++ php/2-comment.php
@@ -0,0 +1,20 @@
+<?php 
+
+# This is the single line comment
+
+
+
+
+// This is also a single line comment.
+
+
+
+/*
+
+This is a comment with multiline
+Github.com/mahdiyarsoft/lang
+
+*/
+
+
+?>
